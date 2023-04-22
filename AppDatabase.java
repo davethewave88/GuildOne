ac0aea9ab6b986fb9cfcb1e7a5452cd306d2b372
davethewave88 <126984488+davethewave88@users.noreply.github.com>
2023-04-22 00:07:13 -0700

    working main activity

---
+++ AppDatabase.java
@@ -0,0 +1,13 @@
+package com.example.guild;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = User.class,version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public static final String DB_NAME = "USER_DATABASE";
+    public static final String USER_TABLE = "USER_TABLE";
+
+    public abstract GuildDAO getGuildDao();
+
+}
---
+++ User.java
@@ -0,0 +1,52 @@
+package com.example.guild;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = AppDatabase.USER_TABLE)
+public class User {
+
+    @PrimaryKey(autoGenerate = true)
+    private int mUserId;
+
+    private String mUserName;
+    private String mPassword;
+
+    public User(String userName, String password) {
+        mUserName = userName;
+        mPassword = password;
+    }
+
+    public int getUserId() {
+        return mUserId;
+    }
+
+    public void setUserId(int userId) {
+        mUserId = userId;
+    }
+
+    public String getUserName() {
+        return mUserName;
+    }
+
+    public void setUserName(String userName) {
+        mUserName = userName;
+    }
+
+    public String getPassword() {
+        return mPassword;
+    }
+
+    public void setPassword(String password) {
+        mPassword = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "mUserId=" + mUserId +
+                ", mUserName='" + mUserName + '\'' +
+                ", mPassword='" + mPassword + '\'' +
+                '}';
+    }
+}
